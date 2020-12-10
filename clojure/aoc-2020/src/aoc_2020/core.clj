9923b92aeeeeaf1102282bc85eae4f6316515839
stindrago <csdcostin@gmail.com>
2020-12-10 16:16:11 +0100

    clojure/aoc-2020/src/aoc_2020/core.clj

---
+++ clojure/aoc-2020/src/aoc_2020/core.clj
@@ -0,0 +1,19 @@
+(ns aoc-2020.core
+  (:gen-class))
+
+(set! *warn-on-reflection* true)
+
+(defn run-problem [n]
+  (let [number (.format (new java.text.DecimalFormat "0") n)
+        solution-function (symbol (str "solution-" number))]
+    (use (vec (list (symbol (str "aoc-2020." number))
+                    :only (list solution-function))))
+    (eval (list solution-function))))
+
+(defn -main
+  "Solution runner"
+  [& args]
+  (if (and args (= 1 (count args)))
+    (println (time (run-problem (Integer/parseInt (first args)))))
+    (println "Usage: lein run [number]")))
+;
