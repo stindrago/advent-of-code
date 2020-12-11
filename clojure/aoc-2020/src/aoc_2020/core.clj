c352693c2cc7361e8caa9d7e618f481bad6aa9c6
stindrago <csdcostin@gmail.com>
2020-12-11 12:37:04 +0100

    clojure/aoc-2020/src/aoc_2020/core.clj

--- clojure/aoc-2020/src/aoc_2020/core.clj
+++ clojure/aoc-2020/src/aoc_2020/core.clj
@@ -4,9 +4,9 @@
 (set! *warn-on-reflection* true)
 
 (defn run-problem [n]
-  (let [number (.format (new java.text.DecimalFormat "0") n)
-        solution-function (symbol (str "solution-" number))]
-    (use (vec (list (symbol (str "aoc-2020." number))
+  (let [day (.format (new java.text.DecimalFormat "0") n)
+        solution-function (symbol (str "solution-" day))]
+    (use (vec (list (symbol (str "aoc-2020." day))
                     :only (list solution-function))))
     (eval (list solution-function))))
 
@@ -15,4 +15,4 @@
   [& args]
   (if (and args (= 1 (count args)))
     (println (time (run-problem (Integer/parseInt (first args)))))
-    (println "Usage: lein run [number]")))
+    (println "Usage: lein run [day]")))
