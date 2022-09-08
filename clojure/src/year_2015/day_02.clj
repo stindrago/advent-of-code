e201bbbc70d5770ced43426fc0f4e9294aed88d6
Costin Dragoi <costin@dragoi.me>
2022-09-08 18:50:47 +0200

    Completed day-02 part-2

--- clojure/src/year_2015/day_02.clj
+++ clojure/src/year_2015/day_02.clj
@@ -20,3 +20,19 @@
               (+ acc (wrapping-paper l w h))))
           0
           (str/split-lines input)))
+
+(defn part-2
+  [input]
+  (reduce (fn [acc curr]
+            (let  [v (map #(Integer/parseInt %)
+                          (str/split curr #"x"))
+                   l (first v)
+                   w (second v)
+                   h (last v)
+                   sorted-v (sort v)
+                   a (first sorted-v)
+                   b (second sorted-v)
+                   shortest-perimeter (+ (* 2 a) (* 2 b))]
+              (+ acc shortest-perimeter (* l w h))))
+          0
+          (str/split-lines input)))
