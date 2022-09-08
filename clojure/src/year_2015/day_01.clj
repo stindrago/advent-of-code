c809c19c0bf277fcc76553a09b9596ab1694b0be
Costin Dragoi <costin@dragoi.me>
2022-09-08 18:47:52 +0200

    Add main function used by babasha script.

--- clojure/src/year_2015/day_01.clj
+++ clojure/src/year_2015/day_01.clj
@@ -55,3 +55,15 @@
          (dec acc))
        (inc pos)
        (rest coll)))))
+
+(defn -main
+  [& args]
+  (let [input (slurp (first args))]
+    (condp some args
+      #(= ":one" %) (part-1 input)
+      #(= ":two" %) (part-2 input)
+      #(= ":both" %) (let [part-1 (part-1 input)
+                           part-2 (part-2 input)]
+                       (println "Answer:"
+                                "\n  part-1:" part-1
+                                "\n  part-2:" part-2)))))
