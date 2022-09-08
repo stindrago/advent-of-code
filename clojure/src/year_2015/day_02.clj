d040c6b5eed739e9a8db33575917f95dbdada9b1
Costin Dragoi <costin@dragoi.me>
2022-09-08 18:50:10 +0200

    Completed day-02 part-1

--- clojure/src/year_2015/day_02.clj
+++ clojure/src/year_2015/day_02.clj
@@ -1,3 +1,22 @@
 (ns year-2015.day-02
   (:require [clojure.string :as str]))
 
+(defn wrapping-paper
+  [l w h]
+  (let [lw (* l w)
+        wh (* w h)
+        hl (* h l)]
+    (+ (* 2 lw) (* 2 wh) (* 2 hl) ; wrapping paper
+       (min lw wh hl)))) ; slack
+
+(defn part-1
+  [input]
+  (reduce (fn [acc curr]
+            (let [v (map #(Integer/parseInt %)
+                         (str/split curr #"x"))
+                  l (first v)
+                  w (second v)
+                  h (last v)]
+              (+ acc (wrapping-paper l w h))))
+          0
+          (str/split-lines input)))
