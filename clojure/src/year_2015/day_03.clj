afb347f1033916c5d65e8e46d7a3d350d16ca709
Costin Dragoi <costin@dragoi.me>
2022-09-09 18:09:57 +0200

    Add day-03 source

---
+++ clojure/src/year_2015/day_03.clj
@@ -0,0 +1,13 @@
+(ns year-2015.day-03)
+
+(defn -main
+  [& args]
+  (let [input (slurp (first args))]
+    (condp some args
+      #(= ":one" %) (part-1 input)
+      #(= ":two" %) (part-2 input)
+      #(= ":both" %) (let [part-1 (part-1 input)
+                           part-2 (part-2 input)]
+                       (println "Answer:"
+                                "\n  part-1:" part-1
+                                "\n  part-2:" part-2)))))
