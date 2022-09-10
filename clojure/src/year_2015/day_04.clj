1cf18307685d9523f39a5a0c19a0574816514892
Costin Dragoi <costin@dragoi.me>
2022-09-10 11:46:15 +0200

    Add day-04 hash function

--- clojure/src/year_2015/day_04.clj
+++ clojure/src/year_2015/day_04.clj
@@ -1,6 +1,15 @@
 (ns year-2015.day-04
   (:require [clj-commons.digest :as digest]))
 
+(defn find-right-hash
+  [input zeros]
+  (loop [curr 0]
+    (println curr)
+    (if (every? zero? (map #(Character/digit % 10)
+                           (take zeros (digest/md5
+                                        (str input curr)))))
+      curr
+      (recur (inc curr)))))
 (defn -main
   [& args]
   (let [input (slurp (first args))]
